e4650998356f8e77ff5a5682c3d76a2bc1bea479
Agniva De Sarker <agnivade@yahoo.co.in>
2015-08-08 03:21:04 +0530

    Added relevant elements to the page
    - Added the elements that are actually going to be required.
    - Cleaned up the css files of unnecessary styles

--- public/scripts/app.jsx
+++ public/scripts/app.jsx
@@ -11,47 +11,16 @@ var ThemeManager = new Mui.Styles.ThemeManager();
 ThemeManager.setTheme(ThemeManager.types.DARK);
 var Colors = Mui.Styles.Colors;
 
+// Component declarations
 var AppBar = Mui.AppBar;
+var List = Mui.List;
+var ListItem = Mui.ListItem;
+var ListDivider = Mui.ListDivider;
+var Avatar = Mui.Avatar;
+var FloatingActionButton = Mui.FloatingActionButton;
+var ToggleStar = Mui.ToggleStar;
 
-var Comment = React.createClass({
-  render: function() {
-      return (
-        <div className="comment">
-        <div className="author">{this.props.author}</div>
-        {this.props.children}
-        </div>
-      )
-      }
-  });
-var CommentList = React.createClass({
-    render: function() {
-        var commentNodes = this.props.data.map(function(comment) {
-          return (
-          <Comment author={comment.author}>
-            {comment.text}
-          </Comment>
-          );
-        });
-        return (
-          <div className="commentList">
-           {commentNodes}
-          </div>
-          );
-      }
-});
-
-var CommentForm = React.createClass({
-    render: function() {
-      return (
-            <div className="commentForm">
-              <br /><br />
-              Hello, world! I am a CommentForm.
-            </div>
-            );
-        }
-});
-
-var CommentBox = React.createClass({
+var Main = React.createClass({
   childContextTypes: {
     muiTheme: React.PropTypes.object
   },
@@ -61,19 +30,48 @@ var CommentBox = React.createClass({
     };
   },
    render: function() {
+    var appBarStyle = {
+      position: 'fixed',
+      top: '0'
+    };
+    var listStyle = {
+      background: 'none',
+      marginTop: '10'
+    };
      return (
-           <div className="commentBox">
-           <AppBar title="Hello There" />
-           <h1>Comments</h1>
-           <CommentList data={data}/>
-           <CommentForm />
+           <div className="main">
+           <AppBar title="Welcome" zDepth={1} style={appBarStyle} />
+            <List subheader="Song Queue" style={listStyle}>
+              <ListItem
+                leftAvatar={<Avatar src="http://img.youtube.com/vi/qcOiJnWniWg/1.jpg" />}
+                primaryText="Brendan Lim"
+                secondaryText={
+                  <p>
+                    <span style={{color: Colors.darkBlack}}>Brunch this weekend?</span><br/>
+                    I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
+                  </p>
+                }
+                secondaryTextLines={2} />
+              <ListDivider inset={true} />
+              <ListItem
+                leftAvatar={<Avatar src="http://img.youtube.com/vi/x1NAhlVRaZ4/1.jpg" />}
+                primaryText="Hey there, this is my Primary Text"
+                secondaryText={
+                  <p>
+                    Do whatever you want, I don't want your secondary Text
+                  </p>
+                }
+                secondaryTextLines={2} />
+              <ListDivider inset={true} />
+            </List>
+            <FloatingActionButton iconClassName="muidocs-icon-content-add-circle"/>
             </div>
             );
   }
 });
 
 React.render(
-  <CommentBox />,
+  <Main />,
   document.getElementById('content')
 );
 
